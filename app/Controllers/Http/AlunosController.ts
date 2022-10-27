2e223f9949ebb77e5f7657476605d96b0fc4b18c
unknown <21114290020@ACADEMICO.IESB.BR>
2022-10-26 21:21:39 -0300

    Validates

--- app/Controllers/Http/AlunosController.ts
+++ app/Controllers/Http/AlunosController.ts
@@ -3,54 +3,53 @@
 import Aluno from "App/Models/Aluno";
 
 export default class AlunosController {
-    async index() {
-      return await Aluno.all();
-    }
-    async store({ request }) {
-      const dados = request.only([
-        "nome",
-        "cpf",
-        "matricula",
-        "salario",
-        "email",
-        "telefone",
-        "cep",
-        "logradouro",
-        "complemento",
-        "numero",
-        "bairro",
-      ]);
-      return await Aluno.create(dados);
-    }
-    async show({ request }) {
-      const id = request.param("id");
-      return await Aluno.findOrFail(id);
-    }
-    async destroy({ request }) {
-      const id = request.param("id");
-      const aluno = await Aluno.findOrFail(id);
-  
-      return await aluno.delete();
-    }
-    async update({ request }) {
-      const id = request.param("id");
-      const aluno = await Aluno.findOrFail(id);
-      const dados = request.only([
-        "nome",
-        "cpf",
-        "matricula",
-        "salario",
-        "email",
-        "telefone",
-        "cep",
-        "logradouro",
-        "complemento",
-        "numero",
-        "bairro",
-      ]);
-  
-      aluno.merge(dados);
-      return await aluno.save();
-    }
+  async index() {
+    return await Aluno.all();
   }
-  
+  async store({ request }) {
+    const dados = request.validate([
+      "nome",
+      "cpf",
+      "matricula",
+      "salario",
+      "email",
+      "telefone",
+      "cep",
+      "logradouro",
+      "complemento",
+      "numero",
+      "bairro",
+    ]);
+    return await Aluno.create(dados);
+  }
+  async show({ request }) {
+    const id = request.param("id");
+    return await Aluno.findOrFail(id);
+  }
+  async destroy({ request }) {
+    const id = request.param("id");
+    const aluno = await Aluno.findOrFail(id);
+
+    return await aluno.delete();
+  }
+  async update({ request }) {
+    const id = request.param("id");
+    const aluno = await Aluno.findOrFail(id);
+    const dados = request.only([
+      "nome",
+      "cpf",
+      "matricula",
+      "salario",
+      "email",
+      "telefone",
+      "cep",
+      "logradouro",
+      "complemento",
+      "numero",
+      "bairro",
+    ]);
+
+    aluno.merge(dados);
+    return await aluno.save();
+  }
+}
